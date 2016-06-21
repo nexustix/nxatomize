4b5138e3986df84ce4bd5e9f8d93399007b1d715
Nexustix <nexus1768@tuta.io>
2016-06-21 19:40:36 +0200

    Add Atom generation when searching

--- nxatomize.go
+++ nxatomize.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"os/user"
 
 	bp "github.com/nexustix/boilerplate"
@@ -12,7 +14,7 @@ func main() {
 	version := "V.0-1-0"
 	fmt.Printf("<-> NxAtomize Version: %s\n", version)
 
-	//args := os.Args
+	args := os.Args
 
 	usr, err := user.Current()
 	bp.FailError(err)
@@ -20,16 +22,74 @@ func main() {
 	//fmt.Printf("<-> Working dir: %s\n", workingDir)
 	fmt.Printf("<-> Home dir: %s\n", workingDir)
 
-	//atomDir :=
-	nrc.InitWorkFolder(workingDir, ".nxreplicator", "atoms")
-	//providerDir :=
-	nrc.InitWorkFolder(workingDir, ".nxreplicator", "providers")
+	atomDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", "atoms")
+	providerDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", "providers")
 
-	/*
-		switch args[0] {
-		case "condition":
+	providers := nrc.ProviderList{Dir: providerDir, Filename: "providers.json"}
+	providers.LoadEntries()
 
+	var providerPath string
+	providerPath = providers.GetEntry(bp.StringAtIndex(1, args)).Path
+
+	atomManager := nrc.AtomManager{WorkingDir: atomDir}
+
+	//switch provider management
+	switch bp.StringAtIndex(1, args) {
+	case "add provider":
+
+		providers.AddEntry(bp.StringAtIndex(2, args), bp.StringAtIndex(3, args))
+		providers.SaveEntries()
+		os.Exit(0)
+	case "del provider":
+		providers.RemoveEntry(bp.StringAtIndex(2, args))
+		providers.SaveEntries()
+	}
+
+	providerID := bp.StringAtIndex(1, args)
+	providerAction := bp.StringAtIndex(2, args)
+	providerQuerry := bp.StringAtIndex(3, args)
+
+	//switch atom management
+	switch providerAction {
+	case "search":
+		//atomDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", path.Join("atoms", providerID))
+		//providerDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", path.Join("providers", bp.StringAtIndex(1, args)))
+		fmt.Printf("search\n")
+
+		//providers.GetEntry(bp.StringAtIndex(1, args)
+		if providers.HasEntry(providerID) {
+			if providerPath != "" {
+				fmt.Printf("</> EXEC >%s %s %s<\n", providerPath, providerAction, providerQuerry)
+
+				//FIXME potentially dangerous if one is careless
+				providerCommand := exec.Command(providerPath, providerAction, providerQuerry)
+				out, err := providerCommand.Output()
+				bp.FailError(err)
+				fmt.Println(string(out))
+
+				//atomManager.SetEntry("curse", nrc.StringToAtom(string(out)))
+				nrc.OutputToAtomsAndAdd("curse", string(out), &atomManager)
+
+			} else {
+				fmt.Printf("<!> ERROR command of '%s' empty\n", providerID)
+			}
+		} else {
+			fmt.Printf("<!> ERROR provider '%s' not found\n", providerID)
 		}
-	*/
-	fmt.Printf("it works !\n")
+
+		//fmt.Println(providers.GetEntry(bp.StringAtIndex(1, args)).Path, "search", "mystcraft")
+		//exec.Command(providers.GetEntry(bp.StringAtIndex(1, args)).Path, "search", "mystcraft")
+
+	case "deps":
+		//atomDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", path.Join("atoms", bp.StringAtIndex(1, args)))
+		//providerDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", path.Join("providers", bp.StringAtIndex(1, args)))
+		fmt.Printf("deps\n")
+
+	case "downinfo":
+		//atomDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", path.Join("atoms", bp.StringAtIndex(1, args)))
+		//providerDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", path.Join("providers", bp.StringAtIndex(1, args)))
+		fmt.Printf("downinfo\n")
+
+	}
+	//fmt.Printf("it works !\n")
 }
